3b68d864f345b5d11dda543701def62fa8d93d5c
chloeperkins3065 <61557333+chloeperkins3065@users.noreply.github.com>
2020-02-27 11:46:22 -0600

    Add files via upload

---
+++ CoinCalculator.cpp
@@ -0,0 +1,58 @@
+/*Chloe Perkins
+CS231 Lab2
+Coin Calculator
+My program will take the user input of how many of each coin the user has and calcualte it 
+and tell the user how much money he has with the coins they input.
+Due: January 30, 2020*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+
+	
+// Welcome message
+    cout << "Welcome to the Coin Cacluator" << endl;
+    char redo = false; 
+	
+
+//Input for the user
+    int pennies;
+    int nickles;
+    int dimes;
+    int quarters;
+    int half_dollars;
+    int one_dollars;
+
+   
+    cout << "Please enter the number of pennies in an amount greater than or equal to zero: " << endl;
+    cin >> pennies;
+    cout << "Please enter the number of nickles in an amount greater than or equal to zero: " << endl;
+    cin >> nickles;
+    cout << "Please enter the number of dimes in an amount greater than or equal to zero: " << endl;
+    cin >> dimes;
+    cout << "PLease enter the number of quarters in an amount greater than or equal to zero: " << endl;
+    cin >> quarters;
+    cout << "Please enter the amount of half-dollar coins, in an amount greater than or equal to zero: " << endl;
+    cin >> half_dollars;
+    cout << "Please enter the amount of one-dollar coins, in an amount greater than or equal to zero: " << endl;
+    cin >> one_dollars;
+
+
+//Restating what the user input to make sure that the quantity was right
+    cout << "You have " << pennies << " pennies" << endl;
+    cout << "You have " << nickles << " nickles" << endl;
+    cout << "You have " << dimes << " dimes" << endl;
+    cout << "You have " << quarters << " quarters" << endl;
+    cout << "You have " << half_dollars << " half-dollar coins" << endl;
+    cout << "You have " << one_dollars << " one-dollar coins" << endl;
+
+
+//calculations
+    float total;
+    total = pennies * .01 + nickles * .05 + dimes * .10 + quarters * .25 + half_dollars * .50 + one_dollars * 1.0;
+    cout << "The value of all of your coins is: $" << total << "." << endl;
+
+
+    
+
+}
